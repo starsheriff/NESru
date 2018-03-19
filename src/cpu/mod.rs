d259a5282bd62687df23e165950c36ad88f342ea
Johannes Weissmann <jo@starsheriff.eu>
2018-03-19 07:08:06 +0100

    refactored cpu module
    refactored cpu module into its own directory to allow further
    refactoring

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,17 +1,11 @@
+mod status_register;
+
+use cpu::status_register::StatusRegisterBits;
+
 use memory::Memory;
 
 use std::fmt::Display;
 
-enum StatusRegisterBits {
-    CarryFlag = 0,        // 0
-    ZeroFlag = 1,         // 2
-    InterruptDisable = 2, // 4
-    DecimalMode = 3,      // 8
-    BreakCommand = 4,     // 16
-    UnusedBit = 5,        // 32
-    OverflowFlag = 6,     // 64
-    NegativeFlag = 7,     // 128
-}
 
 #[derive(Debug)]
 struct StatusRegister {
---
+++ src/cpu/status_register.rs
@@ -0,0 +1,11 @@
+
+pub enum StatusRegisterBits {
+    CarryFlag = 0,        // 0
+    ZeroFlag = 1,         // 2
+    InterruptDisable = 2, // 4
+    DecimalMode = 3,      // 8
+    BreakCommand = 4,     // 16
+    UnusedBit = 5,        // 32
+    OverflowFlag = 6,     // 64
+    NegativeFlag = 7,     // 128
+}
