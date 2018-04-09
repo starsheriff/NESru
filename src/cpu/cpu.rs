fcf049b470baa31915ccb4b9dc96e7c641d8f31f
Johannes Weissmann <jo@starsheriff.eu>
2018-04-10 00:00:23 +0200

    implemented instruction LDA

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -392,6 +392,16 @@ impl CPU {
             // JSR (jump to subrouting)
             0x20 => self.jsr(mem, &OpInfo{mode: Absolute, bytes: 3, cycles: 6}),
 
+            // LDA (load accumulator)
+            0xA9 => self.lda(mem, &OpInfo{mode: Immediate, bytes: 2, cycles: 2}),
+            0xA5 => self.lda(mem, &OpInfo{mode: ZeroPage, bytes: 2, cycles: 3}),
+            0xB5 => self.lda(mem, &OpInfo{mode: ZeroPageX, bytes: 2, cycles: 4}),
+            0xAD => self.lda(mem, &OpInfo{mode: Absolute, bytes: 3, cycles: 4}),
+            0xBD => self.lda(mem, &OpInfo{mode: AbsoluteX, bytes: 3, cycles: 4}),
+            0xB9 => self.lda(mem, &OpInfo{mode: AbsoluteY, bytes: 3, cycles: 4}),
+            0xA1 => self.lda(mem, &OpInfo{mode: IndirectIndexed, bytes: 2, cycles: 6}),
+            0xB1 => self.lda(mem, &OpInfo{mode: IndexedIndirect, bytes: 2, cycles: 5}),
+
             // TODO: more remaining optcodes
             _ => panic!("not implemented"),
         };
@@ -861,9 +871,20 @@ impl CPU {
         self.cycles += opi.cycles;
     }
 
+    /// CPU instruction: LDA (load accumulator)
+    ///
+    /// Loads a byte of memory into the accumulator setting the zero and
+    /// negative flags as appropriate.
     fn lda(&mut self, mem: &mut Memory, opi: &OpInfo) {
-        // TODO
-        panic!("not implemented");
+        let addr = self.get_address(mem, opi.mode).unwrap();
+        let m = mem.read(addr);
+
+        self.accumulator = m;
+        self.update_zero_flag(m);
+        self.update_negative_flag(m);
+
+        self.cycles += opi.cycles;
+        self.program_counter += opi.bytes as u16;
     }
 
     fn ldx(&mut self, mem: &mut Memory, opi: &OpInfo) {
